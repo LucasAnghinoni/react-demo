f2e516b0e81f5a823b5fa957d6840b9c4f8b81f7
Lucas <lucas@jofege.com.br>
2020-03-28 15:37:02 -0300

    mobile da aplicacao

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,13 @@
 const express = require ('express');
 const cors = require ('cors');
 const routes = require ('./routes'); //passar caminho pous é arquivo e nao pacote
-
+const { errors } = require('celebrate');
 const app = express();
 
 app.use(cors());
 // conver em ojbeto js
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
